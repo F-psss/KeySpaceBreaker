eecdf3a49414c95b00acb084ae1540ec7c185f99
Ладейщиков Андрей Денисович <mrflame@MacBook-Air-Ladejsikov.local>
2026-04-09 20:40:47 +0300

    ooophs

--- include/worker_config.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "../include/config.hpp"
-
-app_config::WorkerConfig parse_worker_config(int argc, char** argv);
--- src/client.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <asio.hpp>
-#include <vector>
-#include <string>
-#include "../include/JSON_Protocol.hpp"
-#include "../include/config.hpp"
-
-asio::awaitable<void> run_client(const app_config::ClientConfig& cfg);
